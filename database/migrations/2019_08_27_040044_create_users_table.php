072a056875a377e6992446a8966d933f1637d2b0
aomyimyo <aomyimyo@gmail.com>
2019-08-27 15:03:04 +0700

    สร้าง database

--- database/migrations/2019_08_27_040044_create_users_table.php
+++ database/migrations/2019_08_27_040044_create_users_table.php
@@ -15,6 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('fname');
+            $table->string('lname');
             $table->timestamps();
         });
     }
